ff944612f85d8cc4a3c9b31a732da4e43b1d4796
quanghuynguyen236 <quanghuynguyen236@gmail.com>
2023-03-16 12:24:13 +0700

    Update script

--- produce_run.py
+++ produce_run.py
@@ -1,8 +1,8 @@
 for dataset in ["ecthr_a", "ecthr_b"]:
-    for task in ["base", "add_true_label", "add_false_label"]:
-        for learning_rate in [5e-06, 1e-05, 2e-05, 5e-05]:
-            for train_batch_size in [1, 2, 4, 8]:
-                for seed_number in [43, 97]:
+    for learning_rate in [5e-06, 1e-05, 2e-05, 5e-05]:
+        for train_batch_size in [1, 2, 4, 8]:
+            for seed_number in [43, 97]:
+                for task in ["base", "add_true_label", "add_false_label"]:
                     path = dataset + '/' + task + "/" + f"lr_{learning_rate}_batch_size_{train_batch_size}_seed_{seed_number}"
                     print(f"python longformer_base.py --task_name {task} --dataset {dataset} " + 
                           f"--learning_rate {learning_rate} " + 
